809efa549cd3ee6c48055fbba0179379cde89331
Abhinav Deep <adeep@geduservices.com>
2024-01-08 11:31:31 +0530

    extra

---
+++ ConsoleAppxyz/maxNumber.cs
@@ -0,0 +1,24 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class maxNumber
+{
+    public static void Main(string[] args)
+    {
+
+        int[] arr = new int[] { 11, 23, 43, 84, 75, 6, 57 };
+        int max = arr[0];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        Console.WriteLine("Max number is : " + max);
+
+    }
+}
---
+++ ConsoleAppxyz/sumAverage.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+public class sumAverage
+{
+    public static void Main(string[] args)
+    {
+
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        Console.WriteLine("The sum of num are : " + sum);
+
+        int avg = sum / arr.Length;
+        Console.WriteLine("The average of num are : " + avg);
+
+    }
+}
